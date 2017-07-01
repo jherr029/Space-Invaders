25ecab5923f2a76e92c9eb4b2d605b6cfe51e23f
Jonathan <jherr029@ucr.edu>
2017-06-30 18:12:10 -0700

    added left and right movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,8 @@ import pygame
 from settings import Settings
 from ship import Ship
 
+import game_functions as gf
+
 def run_game():
 
     pygame.init()
@@ -14,17 +16,12 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
 
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        
-        pygame.display.flip()
 
 run_game()
--- ship.py
+++ ship.py
@@ -2,9 +2,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
 
         self.screen = screen
+        self.ai_settings = ai_settings
 
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -12,6 +13,20 @@ class Ship():
 
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        self.center = float(self.rect.centerx)
+    
+        self.moving_right = False
+        self.moving_left  = False
+
+    def update(self):
+        if self.moving_right:
+            self.center += self.ai_settings.ship_speed_factor
+
+        if self.moving_left:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        self.rect.centerx = self.center
 
     def blitme(self):
         
