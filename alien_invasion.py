5c74dd06d8cbc30778df0743dfa540bb0f4c3daa
Jonathan <jherr029@ucr.edu>
2017-07-01 16:49:56 -0700

    can fire bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
 
 import game_functions as gf
 
@@ -17,11 +18,14 @@ def run_game():
 
 
     ship = Ship(ai_settings, screen)
+    bullets = Group()
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
 
 
 run_game()
