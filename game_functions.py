105002f20bc93c4d3a22f1bb60f40c9fd588f517
Jonathan <jherr029@ucr.edu>
2017-07-01 17:00:03 -0700

    refactored bullet function

--- game_functions.py
+++ game_functions.py
@@ -33,9 +33,7 @@ def check_keydown_events(event, ai_settings,screen, ship, bullets):
         ship.moving_left = True
 
     elif event.key == pygame.K_SPACE:
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
-
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
@@ -43,5 +41,15 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
+def update_bullets(bullets):
+    bullets.update()
 
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
